ca32dd5c9fdcf5b70e5901eb1868c3b4c1f62364
honorable-architecture-d4bb <damellfaines8@gmail.com>
2020-12-25 20:26:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,7 @@
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, position)
   if board[position] == " " || board[position] == "" || board[position] == nil
-  	false
+  return	false
 
   else
   	true
