6f254d1c33ec6623fd9dc7d494a0405aa85f8cfe
honorable-architecture-d4bb <damellfaines8@gmail.com>
2020-12-25 20:46:33 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,10 @@
 # code your #valid_move? method here
 
-    def position_taken?(array, index)
-      if array[index] == " " || array[index] == "" || array[index] == nil
-  return false
-    elsif array[index] == "X" || array[index] == "O"
-    return true
-end
-end
+  def valid_move?(array, index)
+    if index.between?(0, 8) && !position_taken?(array, index)
+      return true
+    end
+  end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, position)
